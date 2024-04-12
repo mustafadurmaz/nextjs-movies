04f28e2e17217b31414e1a0d30fceaac77c7a083
mustafadurmaz <mdmz_001@hotmail.com>
2024-04-13 00:27:13 +0300

    movies section componet added

---
+++ src/components/movies-section/index.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+
+import styles from "./styles.module.css";
+
+function MoviesSection({ title, movies }: any) {
+  return (
+    <div className={styles.moviesSection}>
+      <h3 className={styles.title}>{title}</h3>
+      <div className={styles.movies}>
+        {movies.map((movie: any) => (
+          <div className={styles.movie} key={movie.id}>
+            <Link href={`movie/${movie.id}`}>
+              <Image
+                fill
+                unoptimized
+                alt={movie.title}
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default MoviesSection;
