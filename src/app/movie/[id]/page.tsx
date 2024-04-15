2be16f412a5a3f970d4645a161621741823a4839
mustafadurmaz <mdmz_001@hotmail.com>
2024-04-15 21:19:37 +0300

    movie detail page created

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import MovieContainer from '@/containers/movie';
+import Movies from "@/mocks/movies.json";
+import { notFound } from 'next/navigation';
 
 function MoviePage({params}:any) {
+  const movieDetail= Movies.results.find((movie)=>movie.id === +params.id);
+  
+  if(!movieDetail) {
+    notFound()
+  }
     
   return (
-    <div>MoviePage</div>
+    <div>
+      <MovieContainer movie={movieDetail} />
+    </div>
   )
 }
 
