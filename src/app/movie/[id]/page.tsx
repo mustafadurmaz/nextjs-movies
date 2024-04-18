ae4b16002d781ea4998ee0115da431ea886a547f
mustafadurmaz <mdmz_001@hotmail.com>
2024-04-18 22:27:03 +0300

    loading page created

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -3,11 +3,18 @@ import MovieContainer from '@/containers/movie';
 import Movies from "@/mocks/movies.json";
 import { notFound } from 'next/navigation';
 
-function MoviePage({params}:any) {
+function MoviePage({params, searchParams}:any) {
   const movieDetail= Movies.results.find((movie)=>movie.id === +params.id);
+
+  console.log("searchParams",searchParams);
+  
   
   if(!movieDetail) {
     notFound()
+  }
+
+  if(searchParams.error==="true"){
+    throw new Error("Error happened");
   }
     
   return (
