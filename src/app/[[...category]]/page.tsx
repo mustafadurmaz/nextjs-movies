8c04296d4c39a7e768bcb48c42ffb50e025377fd
mustafadurmaz <mdmz_001@hotmail.com>
2024-04-20 12:09:54 +0300

    catogory page movie find function added

--- src/app/[[...category]]/page.tsx
+++ src/app/[[...category]]/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import HomeContainer from "@/containers/home";
 
-export default function Home() {
+import Movies from "@/mocks/movies.json"
+
+export default function Home({params}:any) {
+    let selectedCategory;
+    if(params?.category?.length>0)
+    selectedCategory=true;
   return (
     <>
-      <HomeContainer />
+      <HomeContainer selectedCategory={{
+        id:params.category?.[0] ?? "",
+        movies:selectedCategory ? Movies.results.slice(0,7) : []
+      }} />
     </>
   );
 }
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import React from "react";
 import Movies from "@/mocks/movies.json";
 import Genres from "@/mocks/genres.json";
-import FeaturedMovie from '@/components/featured-movie';
-import Categories from '@/components/categories';
-import MoviesSection from '@/components/movies-section';
+import FeaturedMovie from "@/components/featured-movie";
+import Categories from "@/components/categories";
+import MoviesSection from "@/components/movies-section";
 
-function HomeContainer() {
+interface Params {
+  selectedCategory: any;
+}
+
+function HomeContainer({ selectedCategory }: Params) {
   return (
     <div>
       <FeaturedMovie movie={Movies.results[0]} />
-      <Categories categories={Genres.genres.slice(0,5)} />
-      <MoviesSection title="Popular Films" movies={Movies.results.slice(1,7)} />
-      <MoviesSection title="Your Favorites" movies={Movies.results.slice(7,13)} />
+      <Categories categories={Genres.genres.slice(0, 5)} />
+      {selectedCategory?.movies?.length > 0 && (
+        <MoviesSection title={Genres.genres.find((genre:any)=>genre.id===+selectedCategory.id)?.name} movies={selectedCategory.movies} />
+      )}
 
+      <MoviesSection
+        title="Popular Films"
+        movies={Movies.results.slice(1, 7)}
+      />
+      <MoviesSection
+        title="Your Favorites"
+        movies={Movies.results.slice(7, 13)}
+      />
     </div>
-  )
+  );
 }
 
-export default HomeContainer
+export default HomeContainer;
